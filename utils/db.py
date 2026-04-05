bae82072fcfee449700a3ef57d9efa7d62cb89c0
royabhrajyoti961-png <royabhrajyoti961@gmail.com>
2026-04-05 22:27:18 +0530

    Update db.py

--- utils/db.py
+++ utils/db.py
@@ -6,6 +6,7 @@ def connect():
 def create_table():
     conn = connect()
     c = conn.cursor()
+
     c.execute("""
         CREATE TABLE IF NOT EXISTS transactions (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,28 +16,36 @@ def create_table():
             date TEXT
         )
     """)
+
     conn.commit()
     conn.close()
 
 def add_transaction(amount, category, note, date):
     conn = connect()
     c = conn.cursor()
-    c.execute("INSERT INTO transactions (amount, category, note, date) VALUES (?, ?, ?, ?)",
-              (amount, category, note, date))
+
+    c.execute(
+        "INSERT INTO transactions (amount, category, note, date) VALUES (?, ?, ?, ?)",
+        (amount, category, note, date)
+    )
+
     conn.commit()
     conn.close()
 
 def get_transactions():
     conn = connect()
     c = conn.cursor()
+
     c.execute("SELECT * FROM transactions")
     data = c.fetchall()
+
     conn.close()
     return data
 
 def delete_transaction(id):
     conn = connect()
     c = conn.cursor()
+
     c.execute("DELETE FROM transactions WHERE id=?", (id,))
     conn.commit()
     conn.close()
